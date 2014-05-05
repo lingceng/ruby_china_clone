3e4b7af252c2e53d0f107ae0f917b592b6ba344a
lingceng <lingceng2008@gmail.com>
2014-05-05 23:11:06 +0800

    user signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,41 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def show
-    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      begin
+        @user = User.find(params[:id])
+      rescue
+        logger.error "Attempt to access invalid user #{params[:id]}"
+        redirect_to root_path, :notice => 'Invalid user'
+      end
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def user_params
+      params.require(:user).permit(:name, :email)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,16 @@ RubyChinaClone::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'topics#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'ui/(:action)', controller: 'ui'
 
+
   resources :topics, only: [:index, :show]
   resources :nodes, only: [:show]
-  resources :users, only: [:show]
+  resources :users, only: [:show,  :new, :create]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
