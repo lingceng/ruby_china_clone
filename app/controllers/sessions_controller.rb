5a95b064bbe24741bd9a3d8aa50bae7b3f015366
lingceng <lingceng2008@gmail.com>
2014-05-11 18:27:11 +0800

    fix readme

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     if User.authenticate(params[:username], params[:password])
       session[:username] = params[:username]
-      redirect_to topics_path
+      redirect_to topics_path, notice: "Login OK"
     else
       redirect_to sign_in_path, alert: "Username or password is not ok"
     end
