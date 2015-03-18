9cfe3d3604f0077308906efbebaffb32736387c1
zhongkx <zhongkx@reocar.com>
2015-03-18 22:57:49 +0800

    Little fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def gravatar_url(email)
     hash = Digest::MD5.hexdigest(email.downcase)
-    "//www.gravatar.com/avatar/#{hash}?s=47"
+    "http://www.gravatar.com/avatar/#{hash}?s=47"
   end
 
   def format_content(content)
